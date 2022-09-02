7ac94e6623b66872d605f9dab4eee7e40a0dd892
Evgeny Kapranov <e.s.kapranov@yandex.ru>
2022-09-02 13:36:56 +0300

    Initial commit

--- logger.py
+++ logger.py
@@ -5,7 +5,7 @@ def logger(filename):
     def _logger(old_function):
         def new_function(*args, **kwargs):
             result = old_function(*args, **kwargs)
-            with open(filename, 'a+') as f:
+            with open(filename, 'a+', encoding='UTF-8') as f:
                 f.write(f'{datetime.datetime.now()} вызвана функция {old_function.__name__} с аргументами {args} и {kwargs}, результат {result}\n')
             # print(f'{datetime.datetime.now()} вызвана функция {old_function.__name__} с аргументами {args} и {kwargs}, результат {result}')
             return result
